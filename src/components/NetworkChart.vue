8ed120664c0f312ec72fb2bb94642496e0a34c6d
Ben W <ben.wilson@mammothgrowth.com>
2022-07-30 18:25:21 -0400

    Work

--- src/components/NetworkChart.vue
+++ src/components/NetworkChart.vue
@@ -7,29 +7,30 @@
         <div class="rounded-xl bg-gray-50 border-indigo-800 border-2 shadow p-5">
             <div class="flow-root">
                 <ul role="list" class="-mb-8">
-                    <li v-for="(event, eventIdx) in timeline" :key="event.id">
+                    <li v-for="(event, eventIdx) in prufer" :key="event.id">
                         <div class="relative pb-8">
-                            <span v-if="eventIdx !== timeline.length - 1"
+                            <span v-if="eventIdx !== prufer.length - 1"
                                 class="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
                             <div class="relative flex space-x-3">
                                 <div>
                                     <span
-                                        :class="[event.iconBackground, 'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white']">
+                                        :class="['bg-indigo-100 h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white']">
+                                        {{ event }}
                                     </span>
                                 </div>
                                 <div class="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
-                                    <RadioGroup v-model="selectedColor">
-                                        <RadioGroupLabel class="block text-sm font-medium text-gray-700">Choose a label
-                                            color</RadioGroupLabel>
+                                    <RadioGroup v-model="prufer[eventIdx]">
+                                        <RadioGroupLabel class="block text-sm font-medium text-gray-700">Choose a number
+                                        </RadioGroupLabel>
                                         <div class="mt-4 flex items-center space-x-3">
-                                            <RadioGroupOption as="template" v-for="color in colors" :key="color.name"
-                                                :value="color" v-slot="{ active, checked }">
+                                            <RadioGroupOption as="template" v-for="val in pruferValues" :key="val.id"
+                                                :value="val" v-slot="{ active, checked }">
                                                 <div
-                                                    :class="[color.selectedColor, active && checked ? 'ring ring-offset-1' : '', !active && checked ? 'ring-2' : '', '-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none']">
-                                                    <RadioGroupLabel as="span" class="sr-only">{{ color.name }}
-                                                    </RadioGroupLabel>
+                                                    :class="[active && checked ? 'ring ring-offset-1' : '', !active && checked ? 'ring-2' : '', '-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none']">
                                                     <span aria-hidden="true"
-                                                        :class="[color.bgColor, 'h-8 w-8 border border-black border-opacity-10 rounded-full']" />
+                                                        :class="['bg-gray-100 h-8 w-8 border border-black border-opacity-10 rounded-full']">
+                                                        {{ val }}
+                                                    </span>
                                                 </div>
                                             </RadioGroupOption>
                                         </div>
@@ -40,159 +41,36 @@
                     </li>
                 </ul>
             </div>
-            <!-- <div>
-                <div class="grid gap-4 grid-cols-8">
-                    <div class="p-2">
-                        <input type="number" name="number"
-                            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                            :placeholder="build.i1" v-model="prufer[0]" />
-                    </div>
-                    <div class="p-2">
-                        <input type="number" name="number"
-                            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                            :placeholder="build.i1" v-model="build.i2" />
-                    </div>
-                    <div class="p-2">
-                        <input type="number" name="number"
-                            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                            :placeholder="build.i1" v-model="build.i3" />
-                    </div>
-                    <div class="p-2">
-                        <input type="number" name="number"
-                            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                            :placeholder="build.i1" v-model="build.i4" />
-                    </div>
-                    <div class="p-2">
-                        <input type="number" name="number"
-                            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                            :placeholder="build.i1" v-model="build.i5" />
-                    </div>
-                    <div class="p-2">
-                        <input type="number" name="number"
-                            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                            :placeholder="build.i1" v-model="build.i6" />
-                    </div>
-                    <div class="p-2">
-                        <input type="number" name="number"
-                            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                            :placeholder="build.i1" v-model="build.i7" />
-                    </div>
-                    <div class="border-b-2 border-l-2 border-gray-900 p-2">
-                        <input type="number" name="number"
-                            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                            :placeholder="build.i1" v-model="build.root" />
-                    </div>
-                </div>
-            </div> -->
         </div>
-        {{ pruferSequence }}
+        <p>{{ prufer }}</p>
+        <p>{{ pruferSequence }}</p>
     </div>
 
 </template>
 
 <script>
-import { ref } from 'vue'
 import { Chart } from 'highcharts-vue'
 import Highcharts from 'highcharts'
 import networkGraphInit from 'highcharts/modules/networkgraph'
 import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue'
 
-
 networkGraphInit(Highcharts)
 
 export default {
-    setup() {
-        const colors = [
-            { name: 'Pink', bgColor: 'bg-pink-500', selectedColor: 'ring-pink-500' },
-            { name: 'Purple', bgColor: 'bg-purple-500', selectedColor: 'ring-purple-500' },
-            { name: 'Blue', bgColor: 'bg-blue-500', selectedColor: 'ring-blue-500' },
-            { name: 'Green', bgColor: 'bg-green-500', selectedColor: 'ring-green-500' },
-            { name: 'Yellow', bgColor: 'bg-yellow-500', selectedColor: 'ring-yellow-500' },
-        ]
-
-        const selectedColor = ref(colors[1])
-    },
     components: {
-        highcharts: Chart
+        highcharts: Chart,
+        RadioGroup: RadioGroup,
+        RadioGroupLabel: RadioGroupLabel,
+        RadioGroupOption: RadioGroupOption
     },
     data() {
         return {
-            prufer: [
-                2,
-                2,
-                6,
-                6,
-                9,
-                5,
-                2
-            ],
-            timeline: [
-                {
-                    id: 1,
-                    content: 'Applied to',
-                    target: 'Front End Developer',
-                    href: '#',
-                    date: 'Sep 20',
-                    datetime: '2020-09-20',
-                    iconBackground: 'bg-gray-400',
-                },
-                {
-                    id: 2,
-                    content: 'Advanced to phone screening by',
-                    target: 'Bethany Blake',
-                    href: '#',
-                    date: 'Sep 22',
-                    datetime: '2020-09-22',
-                    iconBackground: 'bg-blue-500',
-                },
-                {
-                    id: 3,
-                    content: 'Completed phone screening with',
-                    target: 'Martha Gardner',
-                    href: '#',
-                    date: 'Sep 28',
-                    datetime: '2020-09-28',
-                    iconBackground: 'bg-green-500',
-                },
-                {
-                    id: 4,
-                    content: 'Advanced to interview by',
-                    target: 'Bethany Blake',
-                    href: '#',
-                    date: 'Sep 30',
-                    datetime: '2020-09-30',
-                    iconBackground: 'bg-blue-500',
-                },
-                {
-                    id: 5,
-                    content: 'Completed interview with',
-                    target: 'Katherine Snyder',
-                    href: '#',
-                    date: 'Oct 4',
-                    datetime: '2020-10-04',
-                    iconBackground: 'bg-green-500',
-                },
-            ],
-            build: {
-                i1: 1,
-                i2: 1,
-                i3: 1,
-                i4: 1,
-                i5: 1,
-                i6: 1,
-                i7: 1,
-                root: 1,
-            },
+            pruferValues: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            prufer: [2, 2, 6, 6, 9, 5, 2],
             chartOptions: {
                 chart: {
                     type: 'networkgraph',
                 },
-                // title: {
-                //     text: 'The Indo-European Language Tree'
-                // },
-                // subtitle: {
-                //     text: 'A Force-Directed Network Graph in Highcharts'
-                // },
                 plotOptions: {
                     networkgraph: {
                         keys: ['from', 'to'],
@@ -201,6 +79,9 @@ export default {
                             friction: -0.9
                         }
                     }
+                },
+                title: {
+                    text: 'TRIMMER'
                 },
                 series: [{
                     dataLabels: {
